57783c84d362cb8641a16e45edc831c3731dc204
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-01-24 20:34:21 -0600

    rename chia to chives

--- packages/wallets/src/components/did/WalletDIDCreate.tsx
+++ packages/wallets/src/components/did/WalletDIDCreate.tsx
@@ -115,7 +115,7 @@ export default function WalletDIDCreate() {
                 <Typography variant="subtitle1">
                   Enter amount
                 </Typography>
-                <Tooltip title="The amount of Chia you enter must correspond to an even amount of mojos. One additional mojo will be added to the total amount for security purposes.">
+                <Tooltip title="The amount of Chives you enter must correspond to an even amount of mojos. One additional mojo will be added to the total amount for security purposes.">
                   <HelpIcon style={{ color: '#c8c8c8', fontSize: 12 }} />
                 </Tooltip>
               </Flex>
