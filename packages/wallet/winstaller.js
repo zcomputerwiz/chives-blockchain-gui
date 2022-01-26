f29f895c26f29553ffe8c662e20d0fb5eda5f04a
zcomputerwiz <zcomputerwiz@users.noreply.github.com>
2022-01-25 19:03:23 -0600

    fix winstaller app dir

--- packages/wallet/winstaller.js
+++ packages/wallet/winstaller.js
@@ -15,11 +15,11 @@ function getInstallerConfig () {
 
   return Promise.resolve({
     name: "chives-wallet",
-    appDirectory: path.join(rootPath, 'Chives-win32-x64'),
+    appDirectory: path.join(rootPath, 'Chives Light Wallet-win32-x64'),
     authors: 'Hive Project',
     version: process.env.CHIVES_INSTALLER_VERSION,
     noMsi: true,
-    iconUrl: 'https://raw.githubusercontent.com/HiveProject2021/chives-light-wallet/master/electron-react/src/assets/img/chives.ico',
+    iconUrl: 'https://raw.githubusercontent.com/HiveProject2021/chives-light-wallet/main/chives-blockchain-gui/packages/wallet/src/assets/img/chives.ico',
     outputDirectory: path.join(outPath, 'windows-installer'),
     certificateFile: 'win_code_sign_cert.p12',
     certificatePassword: process.env.WIN_CODE_SIGN_PASS,
